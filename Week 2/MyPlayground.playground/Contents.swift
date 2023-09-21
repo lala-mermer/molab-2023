2f2155b3a95b0d4bd5b510be73ad49532fa88f63
lala-mermer <116294345+lala-mermer@users.noreply.github.com>
2023-09-21 09:13:19 -0400

    update

---
+++ Week 2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+// Create text file and copy and paste to add - avoids string quoting requirements
+
+// Load and print one file from Resource bundle
+let path = Bundle.main.path(forResource: "aardvark.txt", ofType: nil)
+let str = try String(contentsOfFile: path!, encoding: .utf8)
+print(str)
+
+// same as a function
+func load(_ file :String) -> String {
+  let path = Bundle.main.path(forResource: file, ofType: nil)
+  let str = try? String(contentsOfFile: path!, encoding: .utf8)
+  return str!
+}
+
+print(load("bat.txt"))
+
+print(load("cat.txt"))
+
+print(load("elephant.txt"))
