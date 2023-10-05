0494ac6cfd535480c68281ec25fe7d7a63f93304
lala-mermer <116294345+lala-mermer@users.noreply.github.com>
2023-10-05 11:53:34 -0400

    meow

---
+++ Week 4/week 04/week 04/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  week 04
+//
+//  Created by Mer Owens on 10/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week 4/week 04/week 04/week_04App.swift
@@ -0,0 +1,17 @@
+//
+//  week_04App.swift
+//  week 04
+//
+//  Created by Mer Owens on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct week_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
