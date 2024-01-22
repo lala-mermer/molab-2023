b4f274027db2e8c344b6e77114b297d6aace3539
lala-mermer <116294345+lala-mermer@users.noreply.github.com>
2024-01-22 18:39:30 -0500

    meow

---
+++ Week03/Week03Demo/Week03Demo/Week03DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week03DemoApp.swift
+//  Week03Demo
+//
+//  Created by Mer Owens on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Week03DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
