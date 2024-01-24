6101884ae30ed59799be7967218649ee4ec0edef
lala-mermer <116294345+lala-mermer@users.noreply.github.com>
2024-01-24 17:21:59 -0500

    meow

---
+++ Week04/Week04-multi-screen/Week04-multi-screen/Week04_multi_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week04_multi_screenApp.swift
+//  Week04-multi-screen
+//
+//  Created by Mer Owens on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Week04_multi_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
