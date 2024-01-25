4c3ef758ed2168bd59dd7a12f1a31020b99b2450
lala-mermer <116294345+lala-mermer@users.noreply.github.com>
2024-01-24 21:49:32 -0500

    meme

---
+++ Week04/Week04-wesplit/Week04-wesplit/Week04_wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week04_wesplitApp.swift
+//  Week04-wesplit
+//
+//  Created by Mer Owens on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Week04_wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
