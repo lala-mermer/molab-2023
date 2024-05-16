fdab98ef0c354fda99810adb76b4a22799cef49b
lala-mermer <116294345+lala-mermer@users.noreply.github.com>
2024-05-16 11:47:45 -0400

    d

---
+++ Week10/MyDiaryWeek10/MyDiaryWeek10/MyDiaryWeek10App.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryWeek10App.swift
+//  MyDiaryWeek10
+//
+//  Created by Mer Owens on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryWeek10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
